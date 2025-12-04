d196f070b1c5551277e1609101c4c322cb6aacf4
Leonor Joaquim <leonorjoaquim98@gmail.com>
2025-12-04 17:46:26 +0000

    First Commit - Start of React Project with Navbar and Footer

---
+++ caes-api/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
