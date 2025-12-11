cd89f633a95af81b07f57fe5c442a950d55f7014
Leonor Joaquim <leonorjoaquim98@gmail.com>
2025-12-11 22:26:58 +0000

    feat(everything): to see on vercel

--- caes-api/src/App.jsx
+++ caes-api/src/App.jsx
@@ -1,17 +1,23 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from './components/Footer'
-
+import Hero from './components/Hero'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Dogs from "./pages/Dog"
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/dogs" element={<Dogs />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
